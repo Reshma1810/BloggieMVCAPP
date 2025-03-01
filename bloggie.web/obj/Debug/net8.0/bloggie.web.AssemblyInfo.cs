fa1629f1cf06174b91462c70ae183688e8adf3c0
Reshma1810 <reshma.dotnetdeveloper@gmail.com>
2025-03-01 20:15:52 +0530

    intial commit with models and controller for bloggie MVC with view

--- bloggie.web/obj/Debug/net8.0/bloggie.web.AssemblyInfo.cs
+++ bloggie.web/obj/Debug/net8.0/bloggie.web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bloggie.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27c13e1640d3ffb72596e5bb9a0e165652c487d")]
 [assembly: System.Reflection.AssemblyProductAttribute("bloggie.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bloggie.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
