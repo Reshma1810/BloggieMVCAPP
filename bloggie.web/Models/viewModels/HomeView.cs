1839e5bcd01101919b7ae66351c85f98db9e6243
Reshma1810 <reshma.dotnetdeveloper@gmail.com>
2025-03-03 18:32:28 +0530

    Added Tag List To Home View Model and Added Get Query For Tag List. Binding Get Api in Home View Page.

---
+++ bloggie.web/Models/viewModels/HomeView.cs
@@ -0,0 +1,11 @@
+﻿using bloggie.web.Models.Domain;
+
+namespace bloggie.web.Models.viewModels
+{
+    public class HomeView
+    {
+        public IEnumerable<Blogpost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+
+    }
+}
